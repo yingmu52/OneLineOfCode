a04cea141c87ebfce37d42b9deb77cb1308b0d74
Xinyi Zhuang <yingmu52@msn.com>
2019-07-16 21:13:17 -0700

    step 1: add vehicle class

--- pop-vs-oop.playground/Contents.swift
+++ pop-vs-oop.playground/Contents.swift
@@ -1,1 +1,29 @@
 import Foundation
+
+class Vehicle {
+	let name: String
+	let numberOfWheels: Int
+	let fuelTankSize: Int
+	let fuelConsumption: Int
+	
+	init(_ name: String, numberOfWheels: Int, fuelTankSize: Int, fuelConsumption: Int) {
+		self.name = name
+		self.numberOfWheels = numberOfWheels
+		self.fuelTankSize = fuelTankSize
+		self.fuelConsumption = fuelConsumption
+	}
+	
+	var info: String {
+		return "\(self.name): can travel: \(self.longestTravelDistance) Km"
+	}
+	
+	var longestTravelDistance: Int {
+		return self.fuelTankSize / self.fuelConsumption * 100
+	}
+}
+
+let honda = Vehicle("Honda", numberOfWheels: 4, fuelTankSize: 50, fuelConsumption: 6)
+let f150 = Vehicle("F150", numberOfWheels: 4, fuelTankSize: 136, fuelConsumption: 13)
+
+print(honda.info)
+print(f150.info)
